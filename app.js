3f1fab3c7194a7496b804960f2364d92b30b4f45
Chirag Chakraborty <67863041+noobchirag69@users.noreply.github.com>
2021-01-08 12:10:53 +0530

    Password hidden..

--- app.js
+++ app.js
@@ -2,14 +2,16 @@ var x = document.getElementById("login");
 var y = document.getElementById("register");
 var z = document.getElementById("btn");
 
-    function register(){
+    function register() {
         x.style.left = "-400px";
         y.style.left = "50px";
         z.style.left = "110px";
     }
 
-    function login(){
+    function login() {
         x.style.left = "50px";
         y.style.left = "450px";
         z.style.left = "0px";
     }
+
+
